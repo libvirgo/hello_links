f4655b35a565e05c3a4f81df258a8e012e833f40
tiandeng <tiandeng@zhuozhuo.io>
2021-12-29 10:30:50 +0800

    [feature] add ok singly-linked stack.

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,4 @@
 #![cfg_attr(debug_assertions, allow(dead_code, unused_imports, unused_variables, unused_mut))]
 
 mod bad_link;
-
-#[cfg(test)]
-mod test {
-    use crate::bad_link::*;
-
-    #[test]
-    fn basics() {
-        let mut list = List::new();
-        assert_eq!(list.pop(), None);
-        list.push(1);
-        list.push(2);
-        list.push(3);
-        assert_eq!(list.pop(), Some(3));
-        assert_eq!(list.pop(), Some(2));
-        list.push(4);
-        list.push(5);
-        assert_eq!(list.pop(), Some(5));
-        assert_eq!(list.pop(), Some(4));
-        assert_eq!(list.pop(), Some(1));
-        assert_eq!(list.pop(), None);
-    }
-}
+mod ok_link;
